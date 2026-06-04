cee06551a4398562ceb0edf5d66d80d8ebcb5354
YayaKane <ykane21pro@gmail.com>
2026-06-03 20:29:56 -0400

    Initial commit

---
+++ app/publications/page.tsx
@@ -0,0 +1,8 @@
+export default function Publications() {
+  return (
+    <main className="p-10">
+      <h1 className="text-3xl font-bold">Lab Publications</h1>
+      <p>List of our research papers will go here.</p>
+    </main>
+  );
+}
